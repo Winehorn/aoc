08c1392b7e3b179ae50c9e38ea22010e11abd169
Florian Kraus <22160065+Winehorn@users.noreply.github.com>
2024-12-01 19:14:47 +0000

    Move solution of day 1 part 1 to function

--- src/aoc24/main.py
+++ src/aoc24/main.py
@@ -1,3 +1,10 @@
+def solve_1_1(first_list: list[int], second_list: list[int]) -> None:
+    first_list.sort()
+    second_list.sort()
+    result = sum([abs(first - second) for first, second in zip(first_list, second_list)])
+    print("Day 1 Part 1")
+    print(result)
+
 if __name__ == "__main__":
     first_list = []
     second_list = []
@@ -6,7 +13,4 @@ if __name__ == "__main__":
             points = line.split()
             first_list.append(int(points[0]))
             second_list.append(int(points[1]))
-    first_list.sort()
-    second_list.sort()
-    result = sum([abs(first - second) for first, second in zip(first_list, second_list)])
-    print(result)
+    solve_1_1(first_list, second_list)
