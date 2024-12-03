c87b05525551b2133216484ebb779c858fdc2ccb
Florian Kraus <22160065+Winehorn@users.noreply.github.com>
2024-12-03 12:08:33 +0000

    Make result calculation reusable

--- src/aoc24/day3.py
+++ src/aoc24/day3.py
@@ -4,6 +4,10 @@ import re
 
 def solve_part1() -> None:
     input = get_data(day=3, year=2024)
+    result = _get_result(input=input, use_conditionals=False)
+    print(result)
+
+def _get_result(input: str, use_conditionals: bool = False) -> int:
     matches = re.findall(pattern=r"mul\((\d{1,3}),(\d{1,3})\)", string=input)
     result = sum([int(match[0])*int(match[1]) for match in matches])
-    print(result)
+    return result
